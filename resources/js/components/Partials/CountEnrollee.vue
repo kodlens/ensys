b1da01b6ade0c2fe14dce3e4f8c254ea930cd156
kodlens <etiennewayne@gmail.com>
2025-05-19 09:30:02 +0800

    add component for count enrollee

---
+++ resources/js/components/Partials/CountEnrollee.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="card">
+        <div class="subtitle">TOTAL NO. OF ENROLMENT</div>
+        <div class="mt-5 title" v-if="info">{{ info.toLocaleString() }}</div>
+        <div class="mt-5 title" v-else>...</div>
+    </div>
+</template>
+
+<script>
+export default {
+	data(){
+		return{
+            info: null,
+
+		}
+	},
+
+	methods:{
+        loadCountEnrolls(){
+            axios.get('/count-enrolls').then(res=>{
+                this.info = res.data
+            }).catch(err=>{
+            
+            })
+        }
+	},
+
+    mounted() {
+        this.loadCountEnrolls()
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
